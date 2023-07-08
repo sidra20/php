46cb79c2dde6e485fcc02ac53830d294d9694363
sidraparacha360 <90301753+sidra20@users.noreply.github.com>
2023-07-08 20:50:14 +0500

    while, do while loop, arrays

--- arrays.php
+++ arrays.php
@@ -1,5 +1,67 @@
 <?php 
+
+//single diemensional array
 $names = array("Sidra","Yusra");
 print_r ($names);
+echo "<br>";
+foreach($names as $name){
+    echo $name ."<br>";
+}
 
+$fruits = array();
+$fruits[0] = "Apple";
+$fruits[1] = "Mango";
+$fruits[2] = "Banana";
+
+echo $fruits[0];
+echo "<br>";
+//print all
+
+for($i=0; $i<sizeof($fruits); $i++){
+    echo $fruits[$i] ."<br>";
+}
+
+echo "<br>";
+foreach($fruits as $fr){
+    echo $fr . "<br>";
+}
+
+
+echo "<br>";
+//associative array
+
+$std_details = array(
+    "name" => "Sidra",
+    "age" => 22,
+    "dob" => "27/sep"
+);
+print_r($std_details);
+echo "<br>";
+
+foreach($std_details as $std){
+    echo $std ."<br>";
+}
+
+//array of array
+$students = array(
+    "std1" => array(
+        "name" => "Sidra",
+        "age" => 22,
+        "designation" => "Faculty" 
+    ),
+    "std2" => array(
+        "name" => "Faiza",
+        "age"=> 24,
+        "desingation" => "Academic Cordinator"
+    )
+
+    );
+
+    echo "<br>";
+
+foreach($students as $stu){
+    foreach($stu as $key => $value){
+        echo $key . " = " . $value ."<br>";
+    }
+}
 ?>
--- loops.php
+++ loops.php
@@ -1,73 +1,73 @@
 <?php 
 //for, while, do while, foreach
 
-for($i=1; $i<=10; $i++){
-    echo $i . "<br>";
-}
-echo "<br>";
-//table
-$table = 5;
-for($i=1;$i<=10;$i++){
-    $res = $table *$i;
-    echo $table . " x " . $i . " = " . $res ."<br>";
-}
+// for($i=1; $i<=10; $i++){
+//     echo $i . "<br>";
+// }
+// echo "<br>";
+// //table
+// $table = 5;
+// for($i=1;$i<=10;$i++){
+//     $res = $table *$i;
+//     echo $table . " x " . $i . " = " . $res ."<br>";
+// }
 
-echo "<br>";
-//sum of 10 natural numbers 
-$sum = 0;
-for($i=1; $i<=10; $i++){
-    $sum = $sum +$i;
-}
-echo $sum;
+// echo "<br>";
+// //sum of 10 natural numbers 
+// $sum = 0;
+// for($i=1; $i<=10; $i++){
+//     $sum = $sum +$i;
+// }
+// echo $sum;
 
-//factorial
-//5x4x3x2x1
-echo "<br>";
-$num = 3;
-$factorial = 1;
-for($i=1; $i<$num;$i++){
-    $factorial = $factorial *($i+1);
-}
-echo $factorial;
+// //factorial
+// //5x4x3x2x1
+// echo "<br>";
+// $num = 3;
+// $factorial = 1;
+// for($i=1; $i<$num;$i++){
+//     $factorial = $factorial *($i+1);
+// }
+// echo $factorial;
 
-//fibonacci series
+// //fibonacci series
 
-echo "<br>";
-$num1=0;
-$num2=1;
-$nextTerm;
-echo $num1 ."<br>". $num2."<br>";
-for($i=1; $i<=10; $i++){
-    $nextTerm = $num1 +$num2;
-    $num1 = $num2;
-    $num2 = $nextTerm;
-    echo $nextTerm . "<br>";
-}
+// echo "<br>";
+// $num1=0;
+// $num2=1;
+// $nextTerm;
+// echo $num1 ."<br>". $num2."<br>";
+// for($i=1; $i<=10; $i++){
+//     $nextTerm = $num1 +$num2;
+//     $num1 = $num2;
+//     $num2 = $nextTerm;
+//     echo $nextTerm . "<br>";
+// }
 
-//nested loop
+// //nested loop
 
-for($i=0;$i<5;$i++){
-    for($j=0;$j<$i;$j++){
-        echo " ";
-    }
-    for($k=0;$k<(2*$i+1);$k++){
-        echo "*";
-    }
-    echo "<br>";
-}
+// for($i=0;$i<5;$i++){
+//     for($j=0;$j<$i;$j++){
+//         echo " ";
+//     }
+//     for($k=0;$k<(2*$i+1);$k++){
+//         echo "*";
+//     }
+//     echo "<br>";
+// }
 
-for($i=1; $i<=5; $i++)
-    {
-        for($j=$i; $j<5-$i-1; $j++)
-        {
-            echo " ";
-        }
-        for($k=1; $k<=(2*$i-1); $k++)
-        {
-            echo "*";
-        }
-        echo "<br>";
-    }
+// for($i=1; $i<=5; $i++)
+//     {
+//         for($j=$i; $j<5-$i-1; $j++)
+//         {
+//             echo " ";
+//         }
+//         for($k=1; $k<=(2*$i-1); $k++)
+//         {
+//             echo "*";
+//         }
+//         echo "<br>";
+//     }
 // for($i=5;$i>=1;$i--){
 //     for($j=1;$j<=$i;$j++){
 //         echo "*";
@@ -137,4 +137,26 @@ $modifiedString = implode('<br>', $words);
 
 // Print the modified string
 echo $modifiedString;
+
+//while
+
+$s = 1;
+while($s<5){
+    echo $s . '<br>';
+    $s++;
+}
+
+echo "<br>";
+$p =1;
+do {
+    echo $p .'<br>';
+    $p++;
+} while($p < 5);
+
+$x = 1;
+
+do {
+  echo "The number is: $x <br>";
+  $x++;
+} while ($x <= 5);
 ?>
